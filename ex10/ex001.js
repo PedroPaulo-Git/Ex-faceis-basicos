924b7662b7153c2f694e205fea8ad6ebff08fadf
PedroPaulo-Git <109928508+PedroPaulo-Git@users.noreply.github.com>
2023-04-19 19:06:31 -0300

    Add files via upload

---
+++ ex10/ex001.js
@@ -0,0 +1,14 @@
+var vel = 74
+console.log(`The speed that s car is ${vel}km/h`)
+if (vel >60 ){
+    
+    console.log(`You ultrapass speed max allowed. FINED!`)
+}
+else{
+    vel<60
+    console.log(`Drive forever using Security Belt!`)
+
+
+}
+//console.log(`You ultrapass speed max allowed. FINED!`)
+//console.log(`Drive forever using Security Belt!`)
---
+++ ex11/ezx004.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log (`You are from ${pais}`)
+if (pais == 'Brasil') {
+    console.log ( 'You are Brazilian ! :)')
+
+}else{
+    console.log( 'You are Stranger! >:( ')
+}
